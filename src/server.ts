51f55b1de98f850891b3cef185b7672c546a6143
Thiago Magalhaes <thiagomagalhaes@O15.local>
2020-09-02 03:36:54 -0300

    10 Commit

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import routes from './routes';
 import { errors } from 'celebrate';
 
 const app = express();
-const port : string|number= process.env.PORT || 3333;
+// const port : string|number= process.env.PORT || 3333;
 
 dotenv.config();
 
@@ -19,7 +19,7 @@ app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(errors());
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Running Server');
 });
 
